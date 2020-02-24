d55a923a1c926e175ee62f38872b669d7bf46d51
pengyang0106 <35582797+pengyang0106@users.noreply.github.com>
2020-02-23 23:55:39 -0800

    0223
    daily learning

---
+++ day1.py
@@ -0,0 +1,16 @@
+#--coding:utf-8--
+print("Hello World\n")
+
+print("Hello Again")
+
+print("i like typing this.")
+
+print ("This is fun.")
+
+print("Yay! Printing.")
+
+print("i'd like much rather you 'not' .")
+
+print("what is 3+2?", 3+2)
+
+print("Is it greater?", 5 > -2)
---
+++ day4.py
@@ -0,0 +1,12 @@
+print ("Marry had a little lamb.")
+
+print("its fleece was white as %s." % 'snow' )
+
+print ("And everywhere that Mart went")
+
+print ("." *10) # what'd that do?
+
+formatter = "%r %r %r %r"
+print (formatter % (1,2,3,4,))
+print (formatter % ("one", "two","three","four"))
+print (formatter % (True,False,False,True))
