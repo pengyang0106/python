26f63c1dc337b20087f23c250468fc91e402d920
pengyang0106 <35582797+pengyang0106@users.noreply.github.com>
2020-02-25 00:14:57 -0800

    new files chanegs
    new files chanegs

---
+++ day7.py
@@ -0,0 +1,6 @@
+age = input("how old are you? " )
+height = input("how tall are you? ")
+weight = input("How much do you weight? ")
+print ("so, you are %r old, %r tall and %r heavy" %(age,height,weight))
+
+#python -m pydoc input
